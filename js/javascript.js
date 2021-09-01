3bdc6592015edf93bf8bd63322233bf5a8745be7
AmeraHamdy <amera.hamdy.ahmed@gmail.com>
2021-09-01 03:54:28 +0200

    route team-solution exam 36

---
+++ js/javascript.js
@@ -0,0 +1,71 @@
+// input
+window.prompt("enter your name");
+
+
+// output
+
+window.alert("amer ahmdy");   type string
+document.getElementById("x").innerHTML="ay tag";
+console.log();
+console.log(typeof());
+
+
+//data type     primative data type
+String "" ''
+ namber    //convert string to number Number();
+ boolean    true false
+undefined  var x;
+null   var x="";
+
+
+if()
+{
+
+}
+else{
+
+}
+
+
+
+if(condition)
+{
+
+}
+else if(){
+
+}
+else if()
+{
+
+}
+
+
+switch()
+{ case x:
+
+
+    breke;
+
+    case y:
+
+    break;
+        
+}
+
+
+for(var i=;i<=number;I++)
+{
+cartona+=ListeningStateChangedEvent;
+}
+
+
+
+operators
+&& ||
++ concatenate
+
+
+
+
+
